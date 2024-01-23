11d0ed52d2904ea0dfaac18183ba6c98fab59d6f
Flavio Mattos <flaviomdev@gmail.com>
2024-01-23 20:58:05 -0300

    Initial Commit

---
+++ React desenvolvendo com JavaScript/organo/src/components/Banner/index.jsx
@@ -0,0 +1,9 @@
+import './banner.css'
+
+export default function Banner(){
+    return (
+        <section className='banner'>
+        <img src="/images/banner.png" alt="Banner da página principal" />
+        </section>
+    )
+}
---
+++ React desenvolvendo com JavaScript/organo/src/components/CampoTexto/index.jsx
@@ -0,0 +1,10 @@
+import './campotexto.css';
+
+export default function CampoTexto(props) {
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input placeholder={props.placeholder} />
+    </div>
+  );
+}
